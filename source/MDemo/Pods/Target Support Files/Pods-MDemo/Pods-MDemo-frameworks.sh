d4849f3a5de696cc5054bf944500a39563d9f4fc
yiqiwang <yiqiwang@tencent.com>
2020-04-07 15:14:19 +0800

    MDemo 安装 pod trunk 的版本

--- source/MDemo/Pods/Target Support Files/Pods-MDemo/Pods-MDemo-frameworks.sh
+++ source/MDemo/Pods/Target Support Files/Pods-MDemo/Pods-MDemo-frameworks.sh
@@ -197,12 +197,10 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../../MFramework.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MFramework/MFramework.framework"
+  install_framework "${PODS_ROOT}/MFramework/MFramework.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../../MFramework.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MFramework/MFramework.framework"
+  install_framework "${PODS_ROOT}/MFramework/MFramework.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
